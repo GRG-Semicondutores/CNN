c6c9ab41142ee0972048ebde84d2465cfc62de21
Felipe Guedes <felipeguedes_oliveira@hotmail.com>
2026-06-30 23:49:40 -0300

    feito bloco de maxPooling

--- conv2d.sv
+++ conv2d.sv
@@ -4,19 +4,27 @@ module Conv2D #(
     parameter KERNEL_SIZE = 3
 ) (
     input logic clk,
-    input logic [10:0] imagem,
+    input logic [DATA_WIDTH-1:0] imagem [0:N_INPUTS-1]
 );
 
 localparam N_INPUTS = KERNEL_SIZE * KERNEL_SIZE; //kernel = N x N
 localparam STAGES = $clog2(N_INPUTS); //estágios de soma pipeline
 
 logic signed [DATA_WIDTH-1:0] kernel [0:N_INPUTS-1];
-logic signed [DATA_WIDTH*2-1:0] soma [0:STAGES][0:N_INPUTS-1];
+logic signed [2*DATA_WIDTH*2-1:0] soma [0:STAGES][0:N_INPUTS-1];
+logic signed [2*DATA_WIDTH-1:0] mult [0:N_INPUTS-1];
 
-logic [1:0] resto;
-
+genvar m;
 genvar s;
 genvar n;
+
+generate
+    for (m = 0; m < N_INPUTS; m = m + 1) begin
+        always @(posedge clk) begin
+            mult[m] <= imagem[m] * kernel[m];
+        end
+    end
+endgenerate
 
 generate
     for (s = 0; s < STAGES; s = s + 1) begin :estagios
@@ -25,7 +33,7 @@ generate
         for (n = 0; n < ((N_INPUTS + (1 << s) - 1) >> s) >> 1; n = n + 1) begin :elementos_soma 
             always @(posedge clk) begin
                 //primeiro estágio do pipeline de árvore de soma (soma dois do estágio anterior)
-                if (s == 0) begin
+                if (s == 0) begin :primeiro_estagio
                     soma[0][n] <= mult[2 * n] + mult[2 * n + 1];
                 //próximos estágios do pipeline de árvore de soma (soma dois do estágio anterior)
                 end else begin
@@ -33,7 +41,7 @@ generate
                 end
             end
         end
-        
+
         if (s == 0) begin :contorno_primeiro_estagio
             always @(posedge clk) begin
                 //persiste o valor do elemento ímpar (sozinho) para o próximo estágio
---
+++ maxPooling.sv
@@ -0,0 +1,30 @@
+`define MAX2(a,b) (((a) > (b)) ? (a) : (b))
+
+/* MaxPooling pega o maior elemento dentre quatro elementos e joga na saída. Imagem deve ser NxN com N par */
+module maxPooling #(
+    parameter SIDE = 4,
+    parameter DATA_WIDTH = 8
+) (
+    input logic [DATA_WIDTH-1:0] imagem_in [0:N_PIXELS-1],
+    output logic [DATA_WIDTH-1:0] imagem_out [0:N_PIXELS_POOLED-1]
+);
+
+localparam SIDE_POOLED = SIDE/2;
+localparam N_PIXELS = SIDE*SIDE;
+localparam N_PIXELS_POOLED = SIDE_POOLED*SIDE_POOLED;
+
+genvar i;
+genvar j;
+
+generate
+    for (i = 0; i < SIDE; i = i + 2) begin :pool_row
+        for (j = 0; j < SIDE; j = j + 2) begin :pool_col
+            assign imagem_out[((SIDE * i) / 4)  + (j / 2)] = `MAX2(
+                `MAX2(imagem_in[SIDE * (i) + j], imagem_in[SIDE * (i) + j + 1]), 
+                `MAX2(imagem_in[SIDE * (i + 1) + j], imagem_in[SIDE * (i + 1) + j + 1])
+                );
+        end
+    end
+endgenerate
+
+endmodule
